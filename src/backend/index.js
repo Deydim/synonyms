7aa6b272efc0bb3f15980ab6ae6fd60ce2ff8973
Deyan Dimitrov <dey.dimitrov@gmail.com>
2021-06-11 15:40:44 +0300

    clean history and update

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,46 +1,28 @@
-const http = require('http');
-const { lookup } = require('./lookup.js');
+const { createServer } = require('http');
 const axios = require('axios');
 const { URL } = require('url');
+const { lookupRequest } = require('./lookup');
 
-const host = '0.0.0.0';
-// const host = "localhost";
-// const port = 8000;
-const port = process.env.PORT;
+// const host = '0.0.0.0';
+// const port = process.env.PORT;
+const port = 8000;
+const host = "localhost";
 
-const queryTransAPI = async function (word, from, to) {
-  const requestData = lookup(word, from, to);
-  return (
-    await axios(requestData)
-      .then(JSONStringify)
-      .catch(err=>err.toString())
-  )
-}
-
-const JSONStringify = obj => JSON.stringify(obj.data, null, 4);
-
-const requestListener = async function (request, response) {
-  const url = new URL ("http://" +request.headers.host + request.url);
-  const word = url.searchParams.get('word');
-  const from = url.searchParams.get('from');
-  const to = url.searchParams.get('to');
-
-  
-  const result = word && from && to ? await queryTransAPI(word, from, to) : "Bad params in your GET request.";
-  
+const requestListener = async (request, response) => {
   response.setHeader('Access-Control-Allow-Origin', '*');
 	response.setHeader('Access-Control-Request-Method', '*');
 	response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
 	response.setHeader('Access-Control-Allow-Headers', '*');
   response.setHeader('Content-Type', 'application/json; charset=utf-8');
   response.writeHead(200);
-  response.end(result);
+  const url = new URL ("http://" + request.headers.host + request.url);
+  const options = ['word', 'from', 'to'].map(element => url.searchParams.get(element));
+  response.end( 
+    await axios(lookupRequest(options))
+    .then(obj => JSON.stringify(obj.data, null, 4))
+    .catch(err=>err.toString())
+  );
 };
 
-const server = http.createServer(requestListener);
-
-const serverListensAction = () => {
-  console.log(`Server is running on http://${host}:${port}`);
-};
-
-server.listen(port, host, serverListensAction);
+createServer(requestListener)
+  .listen(port, host, () => console.log(`Server is running on http://${host}:${port}`));
